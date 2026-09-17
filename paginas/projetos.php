081ae8479ff9c361114e117e37e391e9332be2b8
Theuzyn2330 <matheusrex2330@gmail.com>
2026-09-17 15:54:01 -0300

    adicionando a api do youtube data v3 nos cards de projeto

--- paginas/projetos.php
+++ paginas/projetos.php
@@ -12,17 +12,17 @@
                     <div class="card-img" style="background-image: url('assets/imagens/projetos/pdv.png');"></div>
                     <div class="card-info">
                         <h3>Empório das Poções</h3>
-                        <p>PDV web em PHP 8+ e MySQL sem frameworks. Aplica MVC, Front Controller e transações PDO para garantir checkout seguro, controle de estoque atômico e preservação de preço histórico por venda.</p>                        </p>
+                        <p>PDV web em PHP 8+ e MySQL sem frameworks. Aplica MVC, Front Controller e transações PDO para garantir checkout seguro, controle de estoque atômico e preservação de preço histórico por venda.</p>           
                         <a href="index.php?page=detalhes_projeto&id=pdv" class="card-link">Acessar Projeto <i class="fas fa-arrow-right"></i></a>
                     </div>
                 </div>
 
                 <div class="project-card">
-                    <div class="card-img" style="background-image: url('assets/imagens/projetos/construcion.jpg');"></div>
+                    <div class="card-img" style="background-image: url('assets/imagens/projetos/api.jpg');"></div>
                     <div class="card-info">
-                        <h3>Em construção</h3>
-                        <p>🚧Coming Soon</p>
-                        <a href="#" class="card-link">Acessar Projeto <i class="fas fa-arrow-right"></i></a>
+                        <h3> Integração com YouTube Data API v3</h3>
+                        <p>Módulo em PHP que consome a YouTube Data API v3 via cURL, carregando as credenciais de forma segura com `.env` para renderizar dinamicamente os cards e players de vídeo na interface com tratamento de erros.</p>
+                       <!-- <a href="#" class="card-link">Acessar Projeto <i class="fas fa-arrow-right"></i></a> -->
                     </div>
                 </div>
 
